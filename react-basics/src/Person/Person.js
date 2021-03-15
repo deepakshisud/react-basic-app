49153f04ab37b37c5335aba5b29c7478ad55869c
Deepakshi <deepakshisud@gmail.com>
2021-03-15 23:22:37 +0530

    State  added

--- react-basics/src/Person/Person.js
+++ react-basics/src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const person = () => {
-    return <p>I'm a person</p>
+    return <p>I'm a personand I am {Math.floor(Math.random()*30)} years old</p>
 };
 
 export default person;
